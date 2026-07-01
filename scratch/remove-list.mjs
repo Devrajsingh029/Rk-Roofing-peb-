15da88c79a021ab9c177dfa626696c5502748d7f
Devraj Singh <devrajsinghmehta@gmail.com>
2026-07-01 20:43:17 +0530

    Revert "feat: redesign Services Included sections into premium card grids across all service pages"
    This reverts commit a2572c09cd762ad6e7ee1466f0328065466b2012.

--- scratch/remove-list.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-let content = fs.readFileSync('services.html', 'utf8');
-content = content.replace(/<ul class="svc-list">[\s\S]*?<\/ul>/g, '');
-fs.writeFileSync('services.html', content, 'utf8');
-console.log('Removed svc-list from services.html');
